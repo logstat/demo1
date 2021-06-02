e34227445eea7b7c15a7f8956d98bbc3d5365d7c
logstat <logstat@126.com>
2021-06-02 13:43:59 +0800

    https test

--- a.py
+++ a.py
@@ -1,2 +1,11 @@
-import os
-print("hello")
+# -*- coding: utf-8 -*-
+
+
+
+for i in range(100):
+    if i % 3 == 1:
+        print("haha")
+    elif i % 3 == 2:
+        print("hehe")
+    else:
+        print("heihei")
